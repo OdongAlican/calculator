7fd846d49c2c6fc17c269a52b782d1511192c1ad
OdongAlican <sandieo.2020@gmail.com>
2020-09-11 02:05:34 +0300

    add handleClick method

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ buttonName, color, wide }) {
+function Button({ buttonName, color, wide, handleClick }) {
   return (
     <div className={wide ? 'btn-style-new' : 'btn-division'}>
-      <button type="button" className={color}>{buttonName}</button>
+      <button type="button" className={color} onClick={() => handleClick(buttonName)}>{buttonName}</button>
     </div>
   );
 }
