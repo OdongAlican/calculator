6a51e134c8d2f9da38cfd6b730461c1348b134de
OdongAlican <sandieo.2020@gmail.com>
2020-09-10 23:08:46 +0300

    add calculate module path in App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../App.css';
+import calculate from '../logic/calculate';
 
 function App() {
   return (
     <div className="App">
       <Display />
       <ButtonPanel />
+      <calculate />
     </div>
   );
 }
